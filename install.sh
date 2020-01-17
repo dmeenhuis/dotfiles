899bb8031a69da352a7c9e1a3b10214959e0682e
Dennis Meenhuis <github@dennus.net>
2020-01-17 13:31:45 +0100

    bat config

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+
+[[ -d ~/.config/bat ]] || mkdir ~/.config/bat
+
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/terminator/config ~/.config/terminator/config
+ln -sf ~/dotfiles/bat/config ~/.config/bat/config
