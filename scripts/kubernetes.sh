429239c7f483cd26499695b480afbc509661b9ad
Dennis Meenhuis <github@dennus.net>
2020-01-17 13:27:12 +0100

    Kubernetes script update. Support for home and end keys in tmux

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -4,7 +4,7 @@ config=`kubectl config view --minify -o=json`
 cluster=`echo $config | jq '.contexts[0].context.cluster' | tr -d \"`
 namespace=`echo $config | jq '.contexts[0].context.namespace' | tr -d \"`
 
-if [ $namespace != "default" ]; then
+if [ $namespace != "default" && $namespace != "null" ]; then
   namespace="/$namespace"
 else
   namespace=""
